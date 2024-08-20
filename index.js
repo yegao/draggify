ac59885a8f35b6422dff118b13eb2e47b8e64245
yegao <familyink@163.com>
2024-08-20 13:37:43 +0800

    2.0.0

--- index.js
+++ index.js
@@ -1,56 +1,139 @@
 let locking = null;
 let lockingClientY = 0;
 let lockingClientX = 0;
+let lockingOffsetTop = 0;
+let lockingOffsetLeft = 0;
+
+let tree = null;
+
+function wrap(fiber, node) {
+	fiber.descendant = node;
+	if (tree === node) {
+		tree = fiber;
+	} else {
+		const ancestor = node.ancestor;
+		if (ancestor && ancestor.descendant === node) {
+			ancestor.descendant = fiber;
+		}
+	}
+	node.ancestor = fiber;
+	node = node.next;
+	let isolate = fiber;
+	while(node) {
+		if (fiber.element.contains(node.element)) {
+			node.ancestor = fiber;
+		} else {
+			isolate.next = node;
+			isolate = node;
+		}
+		node = node.next;
+	} 
+}
+
+function merge(fiber, node) {
+	const fe = fiber.element;
+	let next = node;
+	do {
+		node = next;
+		const element = node.element;
+		if (fe === element) {
+			return;
+		}
+		if (fe.contains(element)) {
+			return wrap(fiber, node);
+		}
+		if (element.contains(fe)) {
+			if (node.descendant) {
+				return merge(fiber, node.descendant);
+			}
+			node.descendant = fiber;
+			fiber.ancestor = node;
+			return;
+		}
+		next = node.next;
+	} while(next);
+	node.next = fiber
+}
+
+function Fiber(element, option) {
+	this.ancestor = null;
+	this.descendant = null;
+	this.next = null;
+	this.element = element;
+	this.option = option;
+}
+
+document.addEventListener('mousedown', (e) => {
+	if (locking) {
+		return;
+	}
+	e.preventDefault();
+	let fiber = tree;
+	const target = e.target;
+	let hit = null;
+	while(fiber) {
+		const element = fiber.element;
+		if (element.contains(target)) {
+			hit = fiber;
+			fiber = fiber.descendant;
+		} else {
+			fiber = fiber.next;
+		}
+	}
+	if (hit) {
+		locking = hit;
+		lockingClientX = e.clientX;
+		lockingClientY = e.clientY;
+		const element = locking.element;
+		lockingOffsetTop = element.offsetTop;
+		lockingOffsetLeft = element.offsetLeft;
+		element.setAttribute('dragging', true);
+		const computedPosition = getComputedStyle(element).getPropertyValue('position');
+		if (computedPosition === 'static') {
+			element.style.setProperty('position', 'absolute');
+			element.style.setProperty('top', lockingOffsetTop + 'px');
+			element.style.setProperty('left', lockingOffsetLeft + 'px');
+		}
+	}
+});
+
+document.addEventListener('mousemove', (e) => {
+	if (locking) {
+		const element = locking.element;
+		const option = locking.option;
+		e.preventDefault();
+		if (option.x) {
+			element.style.setProperty('left', (lockingOffsetLeft + e.clientX - lockingClientX) + 'px');
+		}
+		if (option.y) {
+			element.style.setProperty('top', (lockingOffsetTop + e.clientY - lockingClientY) + 'px');
+		}
+	}
+});
+
+document.addEventListener('mouseup', (e) => {
+	if (locking) {
+		const option = locking.option;
+		const element = locking.element;
+		e.preventDefault();
+		locking = null;
+		lockingClientY = 0;
+		lockingClientX = 0;
+		element.removeAttribute('dragging');
+		if (typeof option.callback === 'function') {
+			requestIdleCallback(() => {
+				option.callback(element);
+			});
+		}
+	}
+});
 
 export default function draggify(element, option = {x: true, y: true, callback: void 0}) {
 	element.setAttribute('draggify', true);
-	let offsetTop = element.offsetTop;
-	let offsetLeft = element.offsetLeft;
-
-	document.addEventListener('mousedown', (e) => {
-		if (locking) {
-			return;
-		}
-		e.preventDefault();
-		if (element.contains(e.target)) {
-			locking = element;
-			element.setAttribute('dragging', true);
-			const computedPosition = getComputedStyle(element).getPropertyValue('position');
-			if (computedPosition === 'static') {
-				element.style.setProperty('position', 'absolute');
-				element.style.setProperty('top', offsetTop + 'px');
-				element.style.setProperty('left', offsetLeft + 'px');
-			}
-			offsetTop = element.offsetTop;
-			offsetLeft = element.offsetLeft;
-			lockingClientX = e.clientX;
-			lockingClientY = e.clientY;
-		}
-	});
-
-	document.addEventListener('mousemove', (e) => {
-		if (locking === element) {
-			e.preventDefault();
-			if (option.x) {
-				locking.style.setProperty('left', (offsetLeft + e.clientX - lockingClientX) + 'px');
-			}
-			if (option.y) {
-				locking.style.setProperty('top', (offsetTop + e.clientY - lockingClientY) + 'px');
-			}
-		}
-	});
-
-	document.addEventListener('mouseup', (e) => {
-		if (locking === element) {
-			e.preventDefault();
-			const element = locking;
-			locking = null;
-			lockingClientY = 0;
-			lockingClientX = 0;
-			element.removeAttribute('dragging');
-			if (typeof option.callback === 'function') {
-				requestIdleCallback(option.callback);
-			}
-		}
-	});
+	const fiber = new Fiber(element, option);
+	if (tree) {
+		merge(fiber, tree);
+	} else {
+		tree = fiber;
+	}
 }
